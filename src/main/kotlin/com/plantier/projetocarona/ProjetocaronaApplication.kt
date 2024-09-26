fb3e5ff84a3c1fc6fc5a300df7ddaaa377e428d6
Junior Plantier <junior.plantier@gmail.com>
2024-09-26 14:35:41 -0300

    commit inicial

---
+++ src/main/kotlin/com/plantier/projetocarona/ProjetocaronaApplication.kt
@@ -0,0 +1,11 @@
+package com.plantier.projetocarona
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjetocaronaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjetocaronaApplication>(*args)
+}
---
+++ src/test/kotlin/com/plantier/projetocarona/ProjetocaronaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.plantier.projetocarona
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjetocaronaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
